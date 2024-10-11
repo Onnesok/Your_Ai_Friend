d25b797fca68267ce338eb8e6384ae452b74fa09
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-10-11 17:16:03 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:your_ai_friend/chat_page/chat_page.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-Future<void> main() async {
+void main() async {
   await dotenv.load(fileName: 'api_key.env');
-  Gemini.init(apiKey: dotenv.env['GEMINI_API_KEY'] ?? '');
-  //Gemini.init(apiKey: GEMINI_API_KEY);
   WidgetsFlutterBinding.ensureInitialized();
-
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  Widget initialPage;
+  runApp(MyApp());
+}
 
-  runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.orange,
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toggleTheme(bool isDarkMode) {
+    setState(() {
+      _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.red,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(color: Colors.black),
         ),
-        navigatorKey: navigatorKey,
-        home: ChatAI(),
+        scaffoldBackgroundColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: Colors.grey[200],
+          hintStyle: TextStyle(color: Colors.black),
+        ),
       ),
-  );
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Colors.red,
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color(0xFF121212),
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: const Color(0xFF1E1E1E),
+          hintStyle: TextStyle(color: Colors.grey[400]),
+        ),
+      ),
+      themeMode: _themeMode,
+      navigatorKey: navigatorKey,
+      home: ChatAI(onThemeChanged: _toggleTheme),
+    );
+  }
 }
